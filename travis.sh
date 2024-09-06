ccaeef3b83a1669da2b69eef4f5833255c393a91
Rick Brown <ricksbrown73@gmail.com>
2024-09-06 13:39:52 +1000

    Remove old CI files
    Actually just wanting to trigger sonarcloud scan because a lot of
    projects I work on seem to have vanished, want to see what happens.

--- travis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn -P coverage clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -Dsonar.projectKey=ricksbrown_cowsay -Dsonar.sources=. -Dsonar.inclusions=src/main/**
